103fe8c2608a55ef5630448719e4e7021095a0c6
Shr Ying Pan <apppeterpan@gmail.com>
2018-10-19 01:27:59 +0800

    Initial Commit

---
+++ TimesCar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimesCar
+//
+//  Created by SHIH-YING PAN on 2018/10/19.
+//  Copyright © 2018 SHIH-YING PAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
